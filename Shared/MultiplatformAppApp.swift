0a8086296e6fbb7b8bd6b425400ad285de8e9abe
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2021-09-29 15:43:07 +0800

    Initial Commit

---
+++ Shared/MultiplatformAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplatformAppApp.swift
+//  Shared
+//
+//  Created by jjaychen on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct MultiplatformAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
